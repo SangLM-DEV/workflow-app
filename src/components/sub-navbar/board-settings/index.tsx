9e1cedfc256711eeaa92e875cf6397e46e7cb8df
Lại Minh Sáng <kaidev99@Kais-MacBook-Pro.local>
2023-06-14 12:57:22 +0700

    modified:   src/components/sub-navbar/board-settings/index.tsx

--- src/components/sub-navbar/board-settings/index.tsx
+++ src/components/sub-navbar/board-settings/index.tsx
@@ -28,16 +28,13 @@ import {
   saveBoard,
   fetchBoard,
   deleteBoard
-} from '../../../../src/slices/cards';
+} from '../../../../src/slices/board';
 
 import { AiFillSetting, AiOutlineDelete, AiOutlineCheck } from 'react-icons/ai';
 
 import { useRouter } from 'next/router';
 
-
-
 const BoardSettings = (): JSX.Element => {
-
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const board = useAppSelector((state) => state.board.board);
@@ -50,165 +47,88 @@ const BoardSettings = (): JSX.Element => {
 
   const router = useRouter();
 
-
-
   const handleSave = async () => {
-
     await dispatch(saveBoard());
-
     await dispatch(fetchBoard(board._id));
-
-
-
     onClose();
-
   };
-
-
 
   const handleDelete = async () => {
-
     await dispatch(deleteBoard());
-
-
-
     if (boardDetail.status === 'success') {
-
       router.push('/boards');
-
     }
-
   };
-
-
-
   return (
-
     <>
-
       <Button onClick={onOpen} size="xs" as={Button} m="5px">
-
         <AiFillSetting />
-
       </Button>
 
       <Modal onClose={onClose} isOpen={isOpen} size="xl" isCentered>
-
         <ModalOverlay />
 
         <ModalContent>
-
           <ModalHeader>Cài đặt bảng</ModalHeader>
-
           <ModalCloseButton />
-
           <ModalBody>
-
             <Tabs isFitted variant="enclosed">
-
               <TabList mb="2rem">
-
                 <Tab>Basic</Tab>
 
                 <Tab>Advance</Tab>
-
               </TabList>
-
               <TabPanels>
-
                 <TabPanel>
-
                   <FormControl id="email">
-
                     <FormLabel>Tên bảng</FormLabel>
-
                     <Input
-
                       value={board.name}
-
                       onChange={(e) =>
-
                         dispatch(updateBoardDetail({ type: 'name', value: e.target.value }))
-
                       }
-
                     />
 
                     <FormHelperText>Bạn có thể thay đổi điều này bất cứ lúc nào</FormHelperText>
-
                   </FormControl>
 
                   <Box align="right">
-
                     <Button
-
                       backgroundColor="success"
-
                       color="white"
-
                       onClick={handleSave}
-
                       isLoading={boardDetail.isLoading}>
-
                       <AiOutlineCheck /> &nbsp; Lưu
-
                     </Button>
-
                   </Box>
-
                 </TabPanel>
 
                 <TabPanel>
-
                   <p>Để xóa bảng của bạn, hãy nhấp vào nút Xóa.</p>
 
                   <Box align="right">
-
                     <Button
-
                       bg="red.500"
-
                       color="white"
-
                       onClick={handleDelete}
-
                       _hover={{
-
                         backgroundColor: 'red.600'
-
                       }}
-
                       isLoading={boardDelete}
-
                       loadingText="Deleting">
-
                       <AiOutlineDelete /> &nbsp;Xóa
-
                     </Button>
-
                   </Box>
-
                 </TabPanel>
-
               </TabPanels>
-
             </Tabs>
-
           </ModalBody>
 
           <ModalFooter />
-
         </ModalContent>
-
       </Modal>
-
     </>
-
   );
-
 };
 
-
-
 export default BoardSettings;
-
